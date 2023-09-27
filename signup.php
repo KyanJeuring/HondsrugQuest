c2f20778b5681d0b7e516c13494460815417dcd3
LocalMerchant <122634244+LocalMerchant@users.noreply.github.com>
2023-09-27 12:04:16 +0200

    hondsrugquest v0.05
    login file en signup file aangepast naar php

--- signup.php
+++ signup.php
@@ -20,7 +20,7 @@
         </ul>
         <ul id="navBarRight">
             <li><a class="active" href="./signup.html">Sign up</a></li>
-            <li><a href="./login.html">Login</a></li>
+            <li><a href="./inlog.php">Login</a></li>
             
         </ul>
         </nav>
