59034a6f3620c7b4e7bcee1d63d35421a224ffb4
KPPJeuring <Kyanjeuring@gmail.com>
2023-10-04 17:28:59 +0200

    HondsrugQuest v0.06a
    Email input toegevoegd in inlog.php en signup.php en maxlength verwijderd.

--- inlog.php
+++ inlog.php
@@ -1,72 +1,79 @@
 <html lang="nl">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title> Login</title>
-        <link rel="icon" href="assets/favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-        <link rel="stylesheet" type="text/css" href="css/navBar.css" />
-   
-    </head>
-    <body>
-        <nav>
-            <ul id="navBarLeft">
-                <li><a  href="./index.html">Home</a></li>
-                <li><a href="./leaderboard.html">Leaderboard</a></li>
-            </ul>
-            <ul id="navBarRight">
-                <li><a href="./signup.php">Sign up</a></li>
-                <li><a class="active" href="./inlog.php">Login</a></li>
-            </ul>
-        </nav>
-            <h1 id="pageTitle">Login</h1>
-        <hr>
-        <center>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> Login</title>
+    <link rel="icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="stylesheet" type="text/css" href="css/navBar.css" />
+
+</head>
+
+<body>
+    <nav>
+        <ul id="navBarLeft">
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./leaderboard.html">Leaderboard</a></li>
+        </ul>
+        <ul id="navBarRight">
+            <li><a href="./signup.php">Sign up</a></li>
+            <li><a class="active" href="./inlog.php">Login</a></li>
+        </ul>
+    </nav>
+    <h1 id="pageTitle">Login</h1>
+    <hr>
+    <center>
         <form class="loginform" action="inlog.php" method="post">
-           <h2 id="subTitle">Gebruikersnaam:</h2>
-              <input type="text" name="uName" id="uName" maxlength ="16">
+            <h2 id="subTitle">Gebruikersnaam:</h2>
+            <input type="text" name="uName" id="uName">
+            <br>
+            <h2 id="subTitle">E-mail:</h2>
+            <input type="email" name="Email" id="Email">
             <br>
             <h2 id="subTitle">Wachtwoord:</h2>
-            <input type="password" name="pWord" id="pWord" maxlength ="16">
+            <input type="password" name="pWord" id="pWord">
             <br>
             <input type="submit" value="Inloggen">
-          </form>
-          </center>
-        <h3 id="SubTitle2"> Geen account? Klik <a href="./signup.html">hier</a>.</h3>
-        <?php
-   $servername = "127.0.0.1";
-   $username = "hondsrug_hondsrugquest";
-   $password = "hondsrugquest";
-   $databasename = "hondsrug_hondsrugquest";
-   
-   $inputusername = $_POST['uName'];
-   $inputpassword = $_POST['pWord'];
+        </form>
+    </center>
+    <h3 id="SubTitle2"> Geen account? Klik <a href="./signup.html">hier</a>.</h3>
+    <?php
+  $servername = "127.0.0.1";
+  $username = "hondsrug_hondsrugquest";
+  $password = "hondsrugquest";
+  $databasename = "hondsrug_hondsrugquest";
 
-   // Create connection
-   $conn = new mysqli($servername, $username, $password, $databasename);
-   
-   // Check connection
-   if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-   }
-   echo "Connected successfully";
+  $inputusername = $_POST['uName'];
+  $inputpassword = $_POST['pWord'];
+  $inputemail = $_POST['Email'];
 
-   $sql = "INSERT INTO Inloggegevens (uName, pWord)
-   VALUES (?,?)";
-   try{
-   $stmt = $conn->prepare($sql);
-   $stmt->bind_param("ss", $inputusername, $inputpassword);
-   }catch(exception $ex){
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $databasename);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+
+  $sql = "INSERT INTO Inloggegevens (uName, pWord, Email)
+          VALUES (?,?,?)";
+  try {
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $inputusername, $inputpassword, $inputemail);
+  } catch (exception $ex) {
     var_dump($ex);
-   }
+  }
 
-if ($stmt->execute() === TRUE) {
+  if ($stmt->execute() === TRUE) {
     echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
 
-  $conn->close();   
-        ?> 
-    </body>
+  $conn->close();
+  ?>
+</body>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,19 +1,21 @@
 <html lang="nl">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Signup</title>
-        <link rel="icon" href="assets/favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-        <link rel="stylesheet" type="text/css" href="css/navBar.css" />
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Signup</title>
+    <link rel="icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="stylesheet" type="text/css" href="css/navBar.css" />
     <style>
-        textarea {
-            resize: none;
-        }
+    textarea {
+        resize: none;
+    }
     </style>
-    </head>
-    <body>
-        <nav>
+</head>
+
+<body>
+    <nav>
         <ul id="navBarLeft">
             <li><a href="./index.html">Home</a></li>
             <li><a href="./leaderboard.html">Leaderboard</a></li>
@@ -21,26 +23,30 @@
         <ul id="navBarRight">
             <li><a class="active" href="./signup.php">Sign up</a></li>
             <li><a href="./inlog.php">Login</a></li>
-            
+
         </ul>
-        </nav>
+    </nav>
     <h1 id="pageTitle">Account aanmaken</h1>
     <hr>
     <form class="loginform" method="post" action="signup.php">
         <h2 id="subTitle">Gebruikersnaam:</h2>
-        <input type="text" name="uName" id="uName" maxlength ="16">
+        <input type="text" name="uName" id="uName">
+        <br>
+        <h2 id="subTitle">E-mail:</h2>
+        <input type="email" name="Email" id="Email">
         <br>
         <h2 id="subTitle">Wachtwoord:</h2>
-        <input type="password" name="pWord" id="pWord" maxlength ="16">
+        <input type="password" name="pWord" id="pWord">
         <br>
         <h2 id="subTitle">Wachtwoord herhalen:</h2>
-        <input type="password" maxlength ="16">
+        <input type="password" maxlength="16">
         <br>
         <input type="submit" value="Meld aan">
     </form>
-    <h3 id="SubTitle2"> Al een account? Klik <a href="./login.html">hier</a>.</h3> 
+    <h3 id="SubTitle2"> Al een account? Klik <a href="./login.html">hier</a>.</h3>
     <?php
-    echo $_post["uName"];
+
     ?>
-    </body>
+</body>
+
 </html>
