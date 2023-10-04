364180e580b51a5e5d548ada14a400677f92a26a
LocalMerchant <122634244+LocalMerchant@users.noreply.github.com>
2023-10-04 12:27:49 +0200

    hondsrugquest v0.06
    connectie naar mysql gelegd

--- inlog.php
+++ inlog.php
@@ -22,19 +22,51 @@
             <h1 id="pageTitle">Login</h1>
         <hr>
         <center>
-        <form class="loginform" action="inlog.php">
+        <form class="loginform" action="inlog.php" method="post">
            <h2 id="subTitle">Gebruikersnaam:</h2>
-              <input type="text" id="username" maxlength ="16">
+              <input type="text" name="uName" id="uName" maxlength ="16">
             <br>
             <h2 id="subTitle">Wachtwoord:</h2>
-            <input type="password" maxlength ="16">
+            <input type="password" name="pWord" id="pWord" maxlength ="16">
             <br>
             <input type="submit" value="Inloggen">
           </form>
           </center>
         <h3 id="SubTitle2"> Geen account? Klik <a href="./signup.html">hier</a>.</h3>
         <?php
-        
+   $servername = "127.0.0.1";
+   $username = "hondsrug_hondsrugquest";
+   $password = "hondsrugquest";
+   $databasename = "hondsrug_hondsrugquest";
+   
+   $inputusername = $_POST['uName'];
+   $inputpassword = $_POST['pWord'];
+
+   // Create connection
+   $conn = new mysqli($servername, $username, $password, $databasename);
+   
+   // Check connection
+   if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+   }
+   echo "Connected successfully";
+
+   $sql = "INSERT INTO Inloggegevens (uName, pWord)
+   VALUES (?,?)";
+   try{
+   $stmt = $conn->prepare($sql);
+   $stmt->bind_param("ss", $inputusername, $inputpassword);
+   }catch(exception $ex){
+    var_dump($ex);
+   }
+
+if ($stmt->execute() === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();   
         ?> 
     </body>
 </html>
