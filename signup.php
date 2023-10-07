b1f7a7c55187903164480a48337be22bc7dc9db5
KPPJeuring <Kyanjeuring@gmail.com>
2023-10-07 11:32:38 +0200

    Revert "Hondsrug Quest v0.08"
    This reverts commit c27cf76ba98c89f8e1a7e8f71eb1f5359970feaa.

--- signup.php
+++ signup.php
@@ -12,9 +12,9 @@
 <body>
     <nav>
         <ul id="navBarLeft">
-            <li><a href="./index.php">Home</a></li>
+            <li><a href="./index.html">Home</a></li>
             <li><a href="./Quests.php">Quests</a></li>
-            <li><a href="./leaderboard.php">Leaderboard</a></li>
+            <li><a href="./leaderboard.html">Leaderboard</a></li>
         </ul>
         <ul id="navBarRight">
             <li><a class="active" href="./signup.php">Sign up</a></li>
@@ -37,72 +37,58 @@
         <h2 id="subTitle">Wachtwoord herhalen:</h2>
         <input placeholder="Herhaal uw wachtwoord..." type="password" name="pWord2" id=pWord2>
         <br>
-        <button id="subButton" type="submit">Aanmelden</button>
+        <input type="submit" value="Meld aan" id="submit">
     </form>
     <h3 id="SubTitle2"> Al een account? Klik <a href="./login.html">hier</a>.</h3>
     <?php
-  $servername = "127.0.0.1";
-  $username = "hondsrug_hondsrugquest";
-  $password = "hondsrugquest";
-  $databasename = "hondsrug_hondsrugquest";
+$servername = "127.0.0.1";
+$username = "hondsrug_hondsrugquest";
+$password = "hondsrugquest";
+$databasename = "hondsrug_hondsrugquest";
 
-  $uName = $_POST['uName'];
-  $pWord = $_POST['pWord'];
-  $pWord2 = $_POST['pWord2'];
-  $Email = $_POST['Email'];
+$inputusername = $_POST['uName'];
+$inputpassword = $_POST['pWord'];
+$inputpassword2 = $_POST['pWord2'];
+$inputemail = $_POST['Email'];
 
 
 
-  // Connectie aanmaken
-  $conn = new mysqli($servername, $username, $password, $databasename);
+// Connectie aanmaken
+$conn = new mysqli($servername, $username, $password, $databasename);
 
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (empty($uName)) {
-      echo "Gebruikersnaam is vereist";
-      exit();
-    } elseif (empty($Email)) {
-      echo "Email is vereist";
-      exit();
-    } elseif (empty($pWord)) {
-      echo "Wachtwoord is vereist";
-      exit();
-    } elseif (empty($pWord2)) {
-      echo "Wachtwoord herhalen is vereist";
-      exit();
-    } else {
-      if ($pWord == $pWord2) {
-        echo "Wachtwoorden komen overeen.";
-        header("Location: inlog.php");
-      } else {
-        echo "Wachtwoorden komen niet overeen.";
-        exit();
-      }
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if ($inputpassword == $inputpassword2) {
+    echo "Wachtwoorden komen overeen.";
     }
-  }
-  // Connectie checken
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
+    else {
+        echo "Wachtwoorden komen niet overeen.";
+        die();
+    }
+    }
+// Connectie checken
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
-  $sql = "INSERT INTO Inloggegevens (uName, pWord, Email)
+$sql = "INSERT INTO Inloggegevens (uName, pWord, Email)
         VALUES (?,?,?)";
-  try {
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $uName, $pWord, $Email);
-  } catch (exception $ex) {
-    var_dump($ex);
-  }
+try {
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("sss", $inputusername, $inputpassword, $inputemail);
+} catch (exception $ex) {
+  var_dump($ex);
+}
 
-  if ($stmt->execute() === TRUE) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
+if ($stmt->execute() === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-  $conn->close();
+$conn->close();
 
-  ?>
+    ?>
 </body>
 
 </html>
