c61370d7d2eeff1a5e06f3edc4ab92afc5a9f574
KPPJeuring <Kyanjeuring@gmail.com>
2023-10-10 21:09:53 +0200

    Hondsrug Quest v0.09b
    Sessie aangemaakt bij adminfile.php

--- adminfile.php
+++ adminfile.php
@@ -1,6 +1,9 @@
-<?php 
-session_start(); ?>
-<html>
+<?php
+session_start();
+if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Email'])) {
+?>
+    <html>
+
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,22 +12,29 @@ session_start(); ?>
         <link rel="stylesheet" type="text/css" href="css/index.css" />
         <link rel="stylesheet" type="text/css" href="css/navBar.css" />
     </head>
-    <body>
-    <nav>
-        <ul id="navBarLeft">
-            <li><a href="./index.php">Home</a></li>
-            <li><a href="./Quests.php">Quests</a></li>
-            <li><a href="./leaderboard.php">Leaderboard</a></li>
-        </ul>
-        <ul id="navBarRight">
-            <li><a href="./signup.php">Sign up</a></li>
-            <li><a href="./inlog.php">Login</a></li>
-            <li><a href="./logout.php">Uitloggen</a></li>
-        </ul>
-    </nav>
-    <h1 id="pageTitle">Hondsrug Quest</h1>
-    <h2 id="subTitle">Hallo, <?php echo $_SESSION['uName']; ?>. Uw id: <?php echo $_SESSION['id']; ?></h2>
-    <hr>
-</body>
 
-</html>
+    <body>
+        <nav>
+            <ul id="navBarLeft">
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./Quests.php">Quests</a></li>
+                <li><a href="./leaderboard.php">Leaderboard</a></li>
+            </ul>
+            <ul id="navBarRight">
+                <li><a href="./signup.php">Sign up</a></li>
+                <li><a href="./inlog.php">Login</a></li>
+                <li><a href="./logout.php">Uitloggen</a></li>
+            </ul>
+        </nav>
+        <h1 id="pageTitle">Admin Page</h1>
+        <h2 id="subTitle">Hallo, <?php echo $_SESSION['uName']; ?>.;</h2>
+        <hr>
+    </body>
+
+    </html>
+<?php
+} else {
+    header("Location: inlog.php");
+    exit();
+}
+?>
