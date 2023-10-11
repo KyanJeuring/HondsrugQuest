eabad60f9eaf7e484ec639ebc9f29b4537f5a4a9
LocalMerchant <122634244+LocalMerchant@users.noreply.github.com>
2023-10-11 12:24:27 +0200

    hondsrugquest v0.09e2
    puntkomma verwijderd

--- adminfile.php
+++ adminfile.php
@@ -27,6 +27,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Emai
         <h1 id="pageTitle">Admin Page</h1>
         <h2 id="subTitle">Hallo, <?php echo $_SESSION['uName']; ?>.</h2>
         <hr>
+
+    
     </body>
 
     </html>
