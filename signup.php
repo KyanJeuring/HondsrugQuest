32a02f6fb1ecd52b4b127aa3e2f63d25597a06cf
LocalMerchant <122634244+LocalMerchant@users.noreply.github.com>
2023-11-24 14:56:34 +0100

    hondsrugquest v0.16a
    style aangepast

--- signup.php
+++ signup.php
@@ -40,8 +40,8 @@
 </div>
     <?php
   $servername = "127.0.0.1";
-  //$username = "hondsrug_hondsrugquest";
-  $username = "hondsrugquest@hondsrug.local";
+  $username = "hondsrug_hondsrugquest";
+  //$username = "hondsrugquest@hondsrug.local";
   $password = "hondsrugquest";
   $databasename = "hondsrug_hondsrugquest";
 
