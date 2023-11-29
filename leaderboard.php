d5286dd7583029cd15d79bc377f49e51fbc659d7
Kyan Jeuring <63996544+KyanJeuring@users.noreply.github.com>
2023-11-29 19:05:20 +0100

    Hondsrug Quest v0.19b
    Comments aangepast

--- leaderboard.php
+++ leaderboard.php
@@ -42,24 +42,22 @@ if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Emai
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-            // Telt alle punten op van Quests die een user heeft gedaan en joined de id's aan elkaar die bijelkaar horen behalve de admin user
+            // Telt alle punten op van Quests die een user heeft gedaan en joined de id's aan elkaar die bij elkaar horen behalve voor admin user en op volgorde van hoogste naar laagste aantal punten
             $sql = "SELECT SUM(punten) AS punten, uName FROM Quest INNER JOIN Progress ON Quest.id = Progress.Qid INNER JOIN Inloggegevens ON Inloggegevens.id=Progress.Uid&&Inloggegevens.id!=1 GROUP BY uName ORDER BY punten DESC";
             $result = $conn->query($sql);
-            // if(){
-                if ($result > 0) {
-                    //quests laten zien
-                    while ($row = $result->fetch_assoc()) {
-                        // Laat alle users met punten zien behalve de admin user
-                        if ($row["uName"] == $_SESSION['uName']) {
-                            echo "<div class='QDiv activeUser backgroundOrangeHover'>"."<h1 class='subTitleLB'>". $row["uName"] ."</h1>". "<br>" ."<h2 class='subTitle2LB'>". "Punten: " . $row["punten"]. "</h2>". "</div>";
-                        } else {
-                            echo "<div class='QDiv backgroundOrangeHover'>". "<h1 class='subTitleLB'>". $row["uName"] ."</h1>". "<br>" ."<h2 class='subTitle2LB'>". "Punten: " . $row["punten"]. "</h2>". "</div>";
-                        }
+            if ($result > 0) {
+                //quests laten zien
+                while ($row = $result->fetch_assoc()) {
+                    // Laat alle users met punten zien en geeft een andere stijl voor de user die actief is op de website
+                    if ($row["uName"] == $_SESSION['uName']) {
+                        echo "<div class='QDiv activeUser backgroundOrangeHover'>"."<h1 class='subTitleLB'>". $row["uName"] ."</h1>". "<br>" ."<h2 class='subTitle2LB'>". "Punten: " . $row["punten"]. "</h2>". "</div>";
+                    } else {
+                        echo "<div class='QDiv backgroundOrangeHover'>". "<h1 class='subTitleLB'>". $row["uName"] ."</h1>". "<br>" ."<h2 class='subTitle2LB'>". "Punten: " . $row["punten"]. "</h2>". "</div>";
                     }
-                } else {
-                    echo "Geen gebruikers beschikbaar.";
                 }
-            // }
+            } else {
+                echo "Geen gebruikers beschikbaar.";
+            }
             $conn->close();
     ?>
     </div>
