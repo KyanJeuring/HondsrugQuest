7c6a4762531848aa463dd02b472bf757beaceb83
KPPJeuring <Kyanjeuring@gmail.com>
2023-12-08 20:05:15 +0100

    Hondsrug Quest v0.21b
    Stijl aanpassingen

--- Quests.php
+++ Quests.php
@@ -2,31 +2,31 @@
 session_start();
 if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Email'])) {
 ?>
-<html lang="nl">
+    <html lang="nl">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Quests</title>
-    <link rel="icon" href="assets/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="css/index.css" />
-    <link rel="stylesheet" type="text/css" href="css/navBar.css" />
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Quests</title>
+        <link rel="icon" href="assets/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="stylesheet" type="text/css" href="css/navBar.css" />
+    </head>
 
-<body>
-    <nav>
-        <ul id="navBarLeft">
-            <li><a href="./index.php">Home</a></li>
-            <li><a class="active" href="./Quests.php">Quests</a></li>
-            <li><a href="./leaderboard.php">Leaderboard</a></li>
-        </ul>
-        <ul id="navBarRight">
-            <li><a href="./logout.php">Uitloggen</a></li>
-        </ul>
-    </nav>
-    <h1 class="pageTitle">Quests</h1>
-    <hr>
-    <div>
+    <body>
+        <nav>
+            <ul id="navBarLeft">
+                <li><a href="./index.php">Home</a></li>
+                <li><a class="active" href="./Quests.php">Quests</a></li>
+                <li><a href="./leaderboard.php">Leaderboard</a></li>
+            </ul>
+            <ul id="navBarRight">
+                <li><a href="./logout.php">Uitloggen</a></li>
+            </ul>
+        </nav>
+        <h1 class="pageTitle">Quests</h1>
+        <hr>
+        <div>
         <?php
         require_once("db_config.php");
 
@@ -43,7 +43,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Emai
         if ($result > 0) {
             //quests laten zien
             while ($row = $result->fetch_assoc()) {
-                echo "<a class='QLink' href='./questdetails.php?id=" . $row['id'] . "'>"."<div class='QDiv subTitle'>" . "Titel: " . $row["titel"] . "<br>" . "Punten: " . $row["punten"] . "&nbsp;" . "</div>" . "</a>";
+                echo "<a class='QLink' href='./questdetails.php?id=" . $row['id'] . "'>" . "<div class='QDiv subTitle'>" . $row["titel"] . "<br>" . "Punten: " . $row["punten"] . "&nbsp;" . "</div>" . "</a>";
             }
         } else {
             echo "Geen quests beschikbaar.";
@@ -54,10 +54,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Emai
         exit();
     }
         ?>
-    </div>
-    <footer>
-    <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
-</footer>
-</body>
+        </div>
+        <footer>
+            <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
+        </footer>
+    </body>
 
-</html>
+    </html>
--- questdetails.php
+++ questdetails.php
@@ -14,32 +14,28 @@ if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Emai
 
 ?>
 
-    <html lang="nl">
+<html lang="nl">
+
+<head>
 
     <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Details van de Quest</title>
+        <link rel="icon" href="assets/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="stylesheet" type="text/css" href="css/navBar.css" />
+    </head>
 
-        <head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Details van de Quest</title>
-            <link rel="icon" href="assets/favicon.ico" />
-            <link rel="stylesheet" type="text/css" href="css/index.css" />
-            <link rel="stylesheet" type="text/css" href="css/navBar.css" />
-        </head>
-
-    <body>
-        <nav>
-            <ul id="navBarRight">
-                <li><a href="./Quests.php">Terug</a></li>
-            </ul>
-        </nav>
-        <h1 class="pageTitle">Quests details</h1>
-        <hr>
-        <div>
-            <h2 class="SubTitle">Quest nummer: <?php echo $_GET['id']; ?> </h2>
-           
-
-
+<body>
+    <nav>
+        <ul id="navBarRight">
+            <li><a href="./Quests.php">Terug</a></li>
+        </ul>
+    </nav>
+    <h1 class="pageTitle">Quests details</h1>
+    <hr>
+    <div>
         <?php
         $sql = "SELECT * FROM `Quest` WHERE `id` = ?";
         try {
@@ -53,27 +49,25 @@ if (isset($_SESSION['id']) && isset($_SESSION['uName']) && isset($_SESSION['Emai
         if (true) {
             //quests laten zien
             while ($row = $result->fetch_assoc()) {
-                echo "<center>" . "<h2 class='Subtitle'> Titel: " . $row["titel"] . "</h2>" . "<br>" . "<h2 class='subtitle'> beschrijving:" . $row["beschrijving"] . "</h2>" . "&nbsp;" . "<h3 class='subtitle'> Punten: " . $row["punten"] . "&nbsp;" . "</h3>" . "</center>";
+                echo "<center>" . "<h2 class='pageTitle'>" . $row["titel"] . "</h2>" . "<br>" . "<h2 class='Subtitle'> Locatie: " . $row["locatie"] . "</h2>" . "<br>" .  "<h2 class='subtitle'> Beschrijving:<br></h2>" . "<h2 class='subtitle2'>" . $row["beschrijving"] . "</h2>" . "&nbsp;" . "<h3 class='subtitle'> Punten: " . $row["punten"] . "&nbsp;" . "</h3>" . "</center>";
             }
         } else {
             echo "Geen quests beschikbaar.";
         }
         $conn->close();
-
-        
     } else {
         header("Location: inlog.php");
         exit();
     }
 
         ?>
-        <form action="verifyquest.php?id=<?php echo $_GET['id'];?>" method="POST">
-         <button type="submit" class="SubTitle2"> verifieer quest</button>
+        <form action="verifyquest.php?id=<?php echo $_GET['id']; ?>" method="POST">
+            <button type="submit" class="SubTitle2"> verifieer quest</button>
         </form>
-        </div>
-        <footer>
+    </div>
+    <footer>
         <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
     </footer>
-    </body>
+</body>
 
-    </html>
+</html>
