916c77cab0b89a2aa9c5c48d3559af6a38de9d3c
KPPJeuring <Kyanjeuring@gmail.com>
2024-01-30 21:43:42 +0100

    Hondsrug Quest v0.28
    Feedback optie toegevoegd

--- index.php
+++ index.php
@@ -2,47 +2,48 @@
 session_start();
 if (isset($_SESSION['Uid']) && isset($_SESSION['uName'])) {
 ?>
-    <html lang="nl">
+<html lang="nl">
 
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Hondsrug Quest</title>
-        <link rel="icon" href="assets/favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-        <link rel="stylesheet" type="text/css" href="css/navBar.css" />
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hondsrug Quest</title>
+    <link rel="icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="stylesheet" type="text/css" href="css/navBar.css" />
+</head>
 
-    <body>
-        <header class="header">   
-              <nav>
-                <input class="dropdown-menu" type="checkbox" id="dropdown-menu" />
-                <label class = "menu-icon" for="dropdown-menu"><span class="navicon">
-                </span> </label> 
-                <div class="menu"> 
-                    <ul class="navBarLeft"> 
-                        <li><a class="active" href="./index.php">Home</a></li>
-                        <li><a href="./Quests.php">Quests</a></li>
-                        <li><a href="./leaderboard.php">Leaderboard</a></li>
-                    </ul>            
-                    <ul id="navBarRight"> 
-                        <li><a href="./logout.php">Uitloggen</a></li>
-                    </ul>
-                </div>
-         </nav>
+<body>
+    <header class="header">
+        <nav>
+            <input class="dropdown-menu" type="checkbox" id="dropdown-menu" />
+            <label class="menu-icon" for="dropdown-menu"><span class="navicon">
+                </span> </label>
+            <div class="menu">
+                <ul class="navBarLeft">
+                    <li><a class="active" href="./index.php">Home</a></li>
+                    <li><a href="./Quests.php">Quests</a></li>
+                    <li><a href="./leaderboard.php">Leaderboard</a></li>
+                </ul>
+                <ul id="navBarRight">
+                    <li><a href="./logout.php">Uitloggen</a></li>
+                </ul>
+            </div>
+        </nav>
     </header>
-        <h2 class="pageTitle">Welkom, <?php echo $_SESSION['uName']; ?>. </h2>
-        <hr>
-        <h2 class="SubTitle"> Klik op 'Quests' in het menu om quests te zoeken. Is dit uw eerste keer hier? Klik dan <a href="uitleg.php">hier</a> voor extra uitleg. </h2> 
-        <br>
+    <h2 class="pageTitle">Welkom, <?php echo $_SESSION['uName']; ?>. </h2>
+    <hr>
+    <h2 class="SubTitle"> Klik op 'Quests' in het menu om quests te zoeken. Is dit uw eerste keer hier? Klik dan <a
+            href="uitleg.php">hier</a> voor extra uitleg. </h2>
+    <br>
+    <button onclick='window.location= "https://forms.gle/d6fyWmHQVC6EDqJe9"' class="SubTitle2">Feedback</button>
+    <footer>
+        <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
+    </footer>
+</body>
 
-        <footer>
-            <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
-        </footer>
-    </body>
 
-
-    </html>
+</html>
 <?php
 } else {
     header("Location: inlog.php");
--- uitleg.php
+++ uitleg.php
@@ -14,18 +14,24 @@ if (isset($_SESSION['Uid']) && isset($_SESSION['uName'])) {
     </head>
 
     <body>
-        <header class="header">   
-        <nav>
-            <ul class="navBarLeft"> 
-                <li><a class="active" href="./index.php">Terug</a></li>
-            </ul>            
-        </nav>
-    </header>
+        <header class="header">
+            <nav>
+                <ul class="navBarLeft">
+                    <li><a class="active" href="./index.php">Terug</a></li>
+                </ul>
+            </nav>
+        </header>
         <h2 class="pageTitle">Uitleg</h2>
         <hr>
-        <h2 class="SubTitle">Hallo <?php echo $_SESSION['uName']; ?>,<br>Leuk dat je meedoet aan Hondsrug Quest.<br>Ga naar de lokalen waar de Quests zijn en voltooi ze om zo hoog mogelijk op het Leaderboard te komen. <br>De eerste 5 die alle Quests hebben voltooid kunnen een prijs in ontvangst nemen in het ICT lokaal M219 in de Marke.<br>Deze zoektocht is van xx:xx uur tot xx:xx uur. Hierna wordt een nieuwe ronde gestart.<br>Veel plezier met de Quests!</h2> 
-        <br>
-
+        <h2 class="SubTitle">Hallo <?php echo $_SESSION['uName']; ?>,<br>Leuk dat je meedoet aan Hondsrug Quest.<br>Ga naar
+            de lokalen waar de Quests zijn en voltooi ze om zo hoog mogelijk op het Leaderboard te komen. <br>De eerste 5
+            die alle Quests hebben voltooid kunnen een prijs in ontvangst nemen in het ICT lokaal M219 in de Marke.<br>Deze
+            zoektocht is van xx:xx uur tot xx:xx uur. Hierna wordt een nieuwe ronde gestart.<br>Veel plezier met de Quests!
+        </h2>
+        <br><br>
+        <h2 class="SubTitle">Zou u bij het verlaten van de school nog aan ons feedback willen geven over onze Website?<br>Uw
+            feedback helpt ons om de website te verbeteren.</h2>
+        <button onclick='window.location= "https://forms.gle/d6fyWmHQVC6EDqJe9"' class="SubTitle2">Feedback</button>
         <footer>
             <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
         </footer>
