6701e8c4f08f6928009cba6e701d99ecbd0c7f66
KPPJeuring <Kyanjeuring@gmail.com>
2024-06-27 12:03:02 +0200

    Hondsrug Quest v1.0
    Kleine fixes.

--- inlog.php
+++ inlog.php
@@ -2,42 +2,42 @@
 <html lang="nl">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title> Login</title>
-  <link rel="icon" href="assets/favicon.ico" />
-  <link rel="stylesheet" type="text/css" href="css/index.css" />
-  <link rel="stylesheet" type="text/css" href="css/navBar.css" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> Login</title>
+    <link rel="icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="stylesheet" type="text/css" href="css/navBar.css" />
 </head>
 
 <body>
-  <nav>
-    <ul id="navBarRight">
-      <li><a href="./signup.php">Sign up</a></li>
-      <li><a class="active" href="./inlog.php">Login</a></li>
-    </ul>
-  </nav>
-  <h1 class="pageTitle">Login</h1>
-  <hr>
-  <div class="divBorder">
-    <form id="FInlog" action="inlog.php" method="post">
-      <h2 class="subTitle">Gebruikersnaam:</h2>
-      <input placeholder="Vul uw gebruikersnaam in..." type="text" name="uName" id="uName">
-      <br>
-      <h2 class="subTitle">Wachtwoord:</h2>
-      <input placeholder="Vul uw wachtwoord in..." type="password" name="pWord" id="pWord">
-      <br><br>
-    </form>
-    <button form="FInlog" type="submit" class="SubTitle2">Login!</button>
-    <a href="./mail.php">
-    <button> Wachtwoord vergeten?</button>
-</a>
-    <h3 class="SubTitle2"> Geen account? Klik <a href="./signup.php">hier</a>.</h3>
-  </div>
-  <footer>
-    <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
-  </footer>
-  <?php
+    <nav>
+        <ul id="navBarRight">
+            <li><a href="./signup.php">Sign up</a></li>
+            <li><a class="active" href="./inlog.php">Login</a></li>
+        </ul>
+    </nav>
+    <h1 class="pageTitle">Login</h1>
+    <hr>
+    <div class="divBorder">
+        <form id="FInlog" action="inlog.php" method="post">
+            <h2 class="subTitle">Gebruikersnaam:</h2>
+            <input placeholder="Vul uw gebruikersnaam in..." type="text" name="uName" id="uName">
+            <br>
+            <h2 class="subTitle">Wachtwoord:</h2>
+            <input placeholder="Vul uw wachtwoord in..." type="password" name="pWord" id="pWord">
+            <br><br>
+        </form>
+        <button form="FInlog" type="submit" class="SubTitle2">Login!</button>
+        <a href="./mail.php">
+            <button> Wachtwoord vergeten?</button>
+        </a>
+        <h3 class="SubTitle2"> Geen account? Klik <a href="./signup.php">hier</a>.</h3>
+    </div>
+    <footer>
+        <hr><img src="./assets/HQLogo.png" alt="HondsrugQuestLogo" id="HQLogo">
+    </footer>
+    <?php
   require_once("db_config.php");
 
   // Connectie aanmaken
@@ -94,7 +94,7 @@
           }
         }
       } else {
-        echo "<h2 class='gKN'>" . "gegevens kloppen niet!" . "</h2>";
+        echo "<h2 class='gKN'>" . "Gegevens kloppen niet!" . "</h2>";
       }
     } else {
       print_r($error);
